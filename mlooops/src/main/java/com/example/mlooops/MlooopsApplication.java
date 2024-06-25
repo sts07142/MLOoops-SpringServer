72ef6e7009ec50ff904da65de604bf64bad98e7a
Injae Ryou <sts07142@naver.com>
2024-06-25 21:28:11 +0900

    u

---
+++ mlooops/src/main/java/com/example/mlooops/MlooopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mlooops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlooopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlooopsApplication.class, args);
+	}
+
+}
---
+++ mlooops/src/test/java/com/example/mlooops/MlooopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mlooops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlooopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
